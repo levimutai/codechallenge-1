84a5023669f2c2b33582e17a3067b25a3549b77d
just ivel554 <justivel554@justs-MacBook-Pro.local>
2025-06-19 17:37:40 +0300

    updated index.html and chai.js on chaicalculator

---
+++ chaicalculator/chai.js
@@ -0,0 +1,18 @@
+const userInput = prompt("Karibu! How many cups of Chai Bora would you like to make?");
+const numberOfCups = Number(3);
+
+const calculateChaiIngredients = (numberOfCups) => {
+  const water = 200 * numberOfCups;
+  const milk = 50 * numberOfCups;
+  const tea = 1 * numberOfCups;
+  const sugar = 2 * numberOfCups;
+
+  console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:`);
+  console.log(`Water: ${water} ml`);
+  console.log(`Milk: ${milk} ml`);
+  console.log(`Tea Leaves (Majani): ${tea} tablespoons`);
+  console.log(`Sugar (Sukari): ${sugar} teaspoons`);
+  console.log("\nEnjoy your Chai Bora!");
+};
+
+calculateChaiIngredients(numberOfCups);
